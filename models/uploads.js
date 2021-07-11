cfb4c34d28602733b3799add1634f5e4ad0dcb18
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-07-11 16:42:42 -0400

    updating upload model to include link to faster-loading thumbnails for gallery view

--- models/uploads.js
+++ models/uploads.js
@@ -3,10 +3,11 @@ const Schema = mongoose.Schema;
 
 const uploadSchema = new Schema({
     imageurl: { type: String, required: false },
-    latitude: {type: String, required: false }, 
-    longitude: {type: String, required: false }, 
-    created: {type: String, required: false },
-    
+    thumbnail: { type: String, required: false },
+    latitude: { type: String, required: false },
+    longitude: { type: String, required: false },
+    created: { type: String, required: false },
+
 });
 
 const Upload = mongoose.model('Upload', uploadSchema);
