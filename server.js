45e58c2b9feecc4af9defd3bbf282772573cad96
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-08-23 13:43:43 -0400

    procfile

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 app.set('port', PORT)
 
-app.use(express.static(__dirname +'/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
