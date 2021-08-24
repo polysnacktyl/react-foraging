f30d6d8c9ca3ffdacd3fab34a176aad99438f400
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-08-24 11:06:41 -0400

    deployment futzing

--- server.js
+++ server.js
@@ -9,14 +9,10 @@ const cors = require('cors');
 
 app.set('port', PORT)
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
@@ -38,6 +34,11 @@ mongoose.connect(process.env.MONGODB_URI,
     useCreateIndex: true,
     useFindAndModify: false
   });
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`)
