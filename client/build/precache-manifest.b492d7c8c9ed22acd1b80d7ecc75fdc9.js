31a8f953fddea99ea42cd2245d558c1e4abd3ecb
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-08-26 16:24:10 -0400

    prod environment attempt

---
+++ client/build/precache-manifest.b492d7c8c9ed22acd1b80d7ecc75fdc9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c31582c2644f27f1c0fbec36fb9e3d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ea28a7cfc77c2b45a3",
+    "url": "/static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "13ddd3352b4f2d2422d7",
+    "url": "/static/css/main.3134c42c.chunk.css"
+  },
+  {
+    "revision": "a9ea28a7cfc77c2b45a3",
+    "url": "/static/js/2.14d77988.chunk.js"
+  },
+  {
+    "revision": "ca309bc8a4a28a63a661299a2e6e4f5d",
+    "url": "/static/js/2.14d77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ddd3352b4f2d2422d7",
+    "url": "/static/js/main.8463eb83.chunk.js"
+  },
+  {
+    "revision": "fee2ba555d72fcbdee65",
+    "url": "/static/js/runtime-main.609e913b.js"
+  },
+  {
+    "revision": "baa7e5b3728c47bc41dcc10deccdabde",
+    "url": "/static/media/tester.baa7e5b3.svg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://react-forager.herokuapp.com/precache-manifest.014284c03ab304a0fd15a8f2fe0cf51e.js"
+  "/precache-manifest.b492d7c8c9ed22acd1b80d7ecc75fdc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://react-forager.herokuapp.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
